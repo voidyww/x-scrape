1798277ccc9e8fc9e230e58f4518ced5e96dc3f8
voidyww <yehiawael2005@gmail.com>
2025-04-02 23:50:51 +0200

    Fix port for Railway

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import { chromium } from "playwright";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+
 
 app.use(cors());
 app.use(express.json());
